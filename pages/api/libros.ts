03ec7d200b44646b7f1fac95307e32c892b5e04e
David Avalos <davalos@itparral.edu.mx>
2022-08-10 15:46:36 -0600

    Carta Libros

---
+++ pages/api/libros.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    name: string
+}
+
+export default function handler (req: NextApiRequest, res: NextApiResponse) {
+    // res.status(200).json({ name: 'Example' })
+    switch (req.method) {
+        case 'GET':
+            res.status(200).json({ name2: 'John Doe' })
+            break
+        case 'POST':
+            res.status(201).json({ name: 'John Doe' })
+            break
+        case 'PUT':
+}}

3dd5150fecdc01bbd1667d378dcd6462a839d106
David Avalos <davalos@itparral.edu.mx>
2022-08-11 13:25:56 -0600

    agregar tipos y hacer peticion

--- pages/ej.tsx
+++ pages/ej.tsx
@@ -1,35 +1,30 @@
 import { Grid, Card, Row, Text, Container } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 import { CardLibro } from "../modules/CardLibro";
-
-function Idk() {
-  const click = () => {
-    console.log();
-  }
-
-
-  return (
-    <Card css={{ $$cardColor: '$colors$primary' }}>
-        <Card.Body>
-          <Row justify="center" align="center">
-            <Text h6 size={15} color="white" css={{ m: 0 }}>
-              NextUI gives you the best developer experience with all the features
-              you need for building beautiful and modern websites and
-              applications.
-            </Text>
-          </Row>
-        </Card.Body>
-      </Card>
-  );
-}
+import {Libro} from "../types/tipos";
 
 
 export default function App() {
+  const [libros, setLibros] = useState<Libro[]>([])
+
+  const cargarTodosLibros = async () => {
+    const res = await fetch("/api/libros");
+    const info: Libro[] = await res.json();
+    setLibros(info);
+    console.log(info.length);
+  }
+
+  useEffect(() => {
+    cargarTodosLibros();
+  }, [])
+  
+
   return (
     <Container >
       <Grid.Container gap={3}>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => (
+      {libros.map(i => (
         // <Grid xs={6} md={4} key={i}>
-          <CardLibro key={i}/>
+          <CardLibro key={i.id}/>
         // </Grid>
       ))}
       </Grid.Container>
---
+++ types/tipos.ts
@@ -0,0 +1,7 @@
+export interface Libro {
+    fecha: Date;
+    id: number;
+    link_google: string;
+    link_imagen: string;
+    titulo: string;
+}

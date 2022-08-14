ae142f96be0bbdb5a1d5199e0a9eaf33269f8583
David Avalos <davalos@itparral.edu.mx>
2022-08-13 23:47:45 -0600

    Ahora se puede buscar libros

--- pages/api/libros.ts
+++ pages/api/libros.ts
@@ -9,10 +9,28 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     // res.status(200).json({ name: 'Example' })
     switch (req.method) {
         case 'GET':
-            const connection = await pool.getConnection();
-            const resp = await connection.execute('SELECT * FROM libros');
-            connection.release();
-            res.status(200).json(resp[0]);
+            const {nombre} = req.query;
+            console.log("====================");
+            console.log(nombre);
+            console.log("====================");
+            try{
+                const connection = await pool.getConnection();
+                if (nombre !== undefined) {
+                    const resp = await connection.query(`SELECT * FROM libros WHERE titulo LIKE '%${connection.escape(nombre).replaceAll("'","")}%'`);
+                    connection.release();
+                    res.status(200).json(resp[0]);
+                }
+                else{
+                    const resp = await connection.execute('SELECT * FROM libros LIMIT 100');
+                    connection.release();
+                    res.status(200).json(resp[0]);
+                }
+            }
+            catch (error) {
+                res.status(500).json({error})
+            }
+
+            
             break
         case 'POST':
             res.status(201).json({ name: 'John Doe' })

f060ad40426a4e675dd25821ae04efe0c6273d6a
David Avalos <davalos@itparral.edu.mx>
2022-08-18 15:19:46 -0600

    Cambio a variables de entorno

--- db/config.ts
+++ db/config.ts
@@ -1,11 +1,12 @@
+import "dotenv/config";
 import mysql from 'mysql2/promise';
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'libreria',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,

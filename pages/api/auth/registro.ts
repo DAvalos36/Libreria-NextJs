b766ace671562567e9aea3ac79382105beecc243
David Avalos <davalos@itparral.edu.mx>
2022-08-19 22:42:16 -0600

    Registrar usuarios

---
+++ pages/api/auth/registro.ts
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-anonymous-default-export */
+import type { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from 'bcrypt';
+
+
+import validar from '../../../middlewares/validaciones';
+import {EsquemaRegistroPost, registroPost} from "../../../schemas/registrar"
+import pool from '../../../db/config';
+
+
+
+
+async function registrar(req: NextApiRequest, res: NextApiResponse) {
+    const {usuario, nombre, contra, apellido} = req.body as registroPost;
+    try {
+        const hash = await bcrypt.hash(contra, 10);
+        const connection = await pool.getConnection();
+        const resp = await connection.query("INSERT INTO usuarios (nom_usuario, pass, nombre, apellido) VALUES (?, ?, ?, ?)", [usuario, hash, nombre, apellido]);
+        connection.release();
+        res.end();
+    } catch (error: any ) {
+        if(error.code == "ER_DUP_ENTRY"){
+            res.status(409).json({ status: 'error', error: 'Usuario ya existe' })
+        }
+        res.status(500).json({ status: 'error', error })
+    }
+}
+
+export default validar(registrar, EsquemaRegistroPost);
